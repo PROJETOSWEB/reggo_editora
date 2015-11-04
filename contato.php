24e0968b5884fef592a71267e23c830b1e94342a
Thiago Maneschy <thiago@maneschy.com>
2015-11-04 19:41:51 -0400

    vai

---
+++ contato.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en-US"> <!--<![endif]-->
+<head>
+<!-- head -->
+
+<!-- meta -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>PUBLIQUE • EMPRESAS</title>
+
+
+<link rel="shortcut icon" href="images/ico-reggo.png" type="image/x-icon" />	<script>//<![CDATA[window.mfn_slider_vertical	= { autoplay:0 	};window.mfn_slider_portfolio 	= { autoPlay:0 };//]]></script>
+
+<link rel='stylesheet' id='contact-form-7-css'  href='css/cform.css' type='text/css' media='all' />
+<link rel='stylesheet' id='tp_twitter_plugin_css-css'  href='css/tp_twitter_plugin.css' type='text/css' media='screen' />
+<link rel='stylesheet' id='rs-plugin-settings-css'  href='rs-plugin/css/settings.css' type='text/css' media='all' />
+
+<link rel='stylesheet' id='select2-css'  href='css/select.css' type='text/css' media='all' />
+<link rel='stylesheet' id='woocommerce-layout-css'  href='css/woocommerce-layout.css' type='text/css' media='all' />
+<link rel='stylesheet' id='woocommerce-smallscreen-css'  href='css/woocommerce-smallscreen.css' type='text/css' media='only screen and (max-width: 768px)' />
+<link rel='stylesheet' id='woocommerce-general-css'  href='css/woocommerce.css' type='text/css' media='all' />
+<link rel='stylesheet' id='style-css'  href='css/style.css' type='text/css' media='all' />
+<link rel='stylesheet' id='prettyPhoto-css'  href='css/prettyPhoto.css' type='text/css' media='all' />
+<link rel='stylesheet' id='owl-carousel-css'  href='js/owl-carousel/owl.carousel.css' type='text/css' media='all' />
+<link rel='stylesheet' id='owl-theme-css'  href='js/owl-carousel/owl.theme.css' type='text/css' media='all' />
+<link rel='stylesheet' id='jplayer-css'  href='css/blue.monday/jplayer.blue.monday.css' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-ui-css'  href='css/ui/jquery.ui.all.css' type='text/css' media='all' />
+<link rel='stylesheet' id='responsive-css'  href='css/responsive.css' type='text/css' media='all' />
+<link rel='stylesheet' id='images-green-css'  href='css/skins/green/images.css' type='text/css' media='all' />
+<link rel='stylesheet' id='style-colors-php-css'  href='css/style-colors.css' type='text/css' media='all' />
+<link rel='stylesheet' id='style-php-css'  href='css/style-2.css' type='text/css' media='all' />
+<link rel='stylesheet' id='mfn-woo-css'  href='css/woocommerce.css' type='text/css' media='all' />
+<link rel="stylesheet" href="css/fonts/mfn-icons.css" media="all" />
+<link rel="stylesheet" href="css/custom.css" media="all" />
+
+<link rel='stylesheet' id='Ubuntu-css'  href='http://fonts.googleapis.com/css?family=Ubuntu%3A100%2C300%2C400%2C400italic%2C700&amp;ver=4.2' type='text/css' media='all' />
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,600,900,200' rel='stylesheet' type='text/css' media='all'>
+
+
+
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+
+</head>
+
+<!-- body -->
+<body class="home page page-id-4311 page-parent page-template-default template-slider color-custom layout-full-width header-dark header-alpha sticky-header">
+<!-- #Wrapper -->
+<div id="Wrapper">
+	<!-- #Action_bar 
+	<div id="Action_bar">
+		<ul id="menu-action-menu" class="top-bar-menu">
+			<li class="menu-item  current-menu-item page_item current_page_item "><a href="index.html">Home</a></li>
+			<li class="menu-item  "><a href="help.html">Help Center</a></li>
+			<li class="menu-item  "><a href="about-us.html">About us</a></li>
+			<li class="menu-item  "><a href="contact.html">Contact</a></li>
+			<li class="menu-item  menu-item-10792"><a href="#">Buy it</a></li>
+		</ul>
+		<div class="contact_info">
+			<p class="phone">
+				<i class="icon-phone"></i><a href="#">+61 (0) 3 8376 6284</a>
+			</p>
+			<p class="mail">
+				<i class="icon-mail-line"></i><a href="#">noreply@envato.com</a>
+			</p>
+		</div>
+	</div>-->
+	<!-- #Header -->
+	<header id="Header">
+	<!-- .header_placeholder 4sticky  -->
+	<div class="header_placeholder"></div>	
+			<?php include './menu_reggo.php'; ?>	
+
+	<div id="Subheader-empresas"></div>
+	
+	
+	</header>
+
+
+	<!-- #Content -->
+	<div id="Content">
+		<div class="content_wrapper clearfix">					<!-- .sections_group -->					<div class="sections_group">												<div class="section pad0" style="margin-top: 35px;" >							<div class="section_wrapper clearfix">								<div class="items_group clearfix">									<div class="column two-third column_column">										<h3>MANDE UMA MENSAGEM </h3>										<hr class="hr_left">										<p>											Olá leitor, se você não encontrou a informação que procura											em nosso site ou tem alguma pergunta, comentário, crítica 											ou sugestão a fazer, mande uma mensagem através do											formulário abaixo.										</p>										<div role="form" class="wpcf7" id="wpcf7-f9896-p165-o1" lang="en-US" dir="ltr">											<div class="screen-reader-response">											</div>											<form id="contact-form" class="contact">												<p>													<span class="wpcf7-form-control-wrap name">														<input type="text"  name="name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="SEU NOME"/>													</span>															<span class="wpcf7-form-control-wrap email">														 <input type="text" name="mail" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false" placeholder="SEU EMAIL"/>													</span>															<span class="wpcf7-form-control-wrap subject">														 <input type="text" name="website" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="ASSUNTO"/>													</span>															<span class="wpcf7-form-control-wrap message">														<textarea  name="comment" id="comment" cols="40" rows="6" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="MENSAGEM"></textarea>													</span>													<input type="submit" id="submit_contact" value="ENVIAR" class="wpcf7-form-control wpcf7-submit"/>		                                            <div id="msg" class="message"></div>												<p></p>											</form>										</div>									</div>									<div class="column one-third column_column workinghours">										<div class="widget-area clearfix">											<aside id="text-3" class="widget widget_text">											<div class="textwidget">												<h3>ONDE ESTAMOS</h3>												<hr class="hr_left">												<h5>Reggo Editora</h5>												<h6>Av. Djalma Batista, 1661<br/>												Millenium Bussines Tower - Sala 1308<br/>  												CEP: 69050-010 |  Manaus-AM<br/>												<i class="icon-phone"></i> 92 3308-7327 | editora@reggo.com.br</h6>																								<br/>																								<a href="mailto:editora@reggo.com.br" class="button button_large button_icon"><i class="icon-mail-line"></i></a>												<a href="#" class="button button_large button_icon"><i class="icon-twitter"></i></a>												<a href="#" class="button button_large button_icon"><i class="icon-facebook"></i></a>											</div>											</aside>										</div>									</div>								</div>							</div>						</div>					</div>					<!-- .four-columns - sidebar -->				</div><!-- // CONTENT WRAPPER -->
+
+	<!-- #Footer -->
+			<?php include './footer_reggo.php'; ?>	
+</div><!-- // CONTENT -->
+
+</div><!-- // WRAPPER -->
+
+
+
+<script type='text/javascript' src='js/jquery/jquery.js'></script>
+<script type='text/javascript' src='js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript' src='rs-plugin/js/jquery.themepunch.tools.min.js'></script>
+<script type='text/javascript' src='rs-plugin/js/jquery.themepunch.revolution.min.js'></script>
+
+<script>
+//<![CDATA[
+jQuery(window).load(function(){
+var retina = window.devicePixelRatio > 1 ? true : false;if(retina){var retinaEl = jQuery("#logo img");var retinaLogoW = retinaEl.width();var retinaLogoH = retinaEl.height();retinaEl.attr("src","upload/logo_retina.png").width(retinaLogoW).height(retinaLogoH)}});
+//]]>
+</script>
+	
+
+<script type='text/javascript' src='js/jquery.form.min.js'></script>
+
+<script type='text/javascript' src='js/frontend/add-to-cart.min.js'></script>
+<script type='text/javascript' src='js/select2/select2.min.js'></script>
+<script type='text/javascript' src='js/jquery-blockui/jquery.blockUI.min.js'></script>
+<script type='text/javascript' src='js/frontend/woocommerce.min.js'></script>
+<script type='text/javascript' src='js/jquery-cookie/jquery.cookie.min.js'></script>
+
+<script type='text/javascript' src='js/frontend/cart-fragments.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/core.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/widget.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/mouse.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/sortable.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/tabs.min.js'></script>
+<script type='text/javascript' src='js/jquery/ui/accordion.min.js'></script>
+<script type='text/javascript' src='js/owl-carousel/owl.carousel.min.js'></script>
+<script type='text/javascript' src='js/jquery.jplayer.min.js'></script>
+<script type='text/javascript' src='js/jquery.plugins.js'></script>
+<script type='text/javascript' src='js/mfn.menu.js'></script>
+<script type='text/javascript' src='js/scripts.js'></script>
+
+
+
+</body>
+</html>
